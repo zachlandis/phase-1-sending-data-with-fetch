2d372bc6fdcbf56c2f8befca607a849951b4c451
Zach <zachlandis91@gmail.com>
2023-01-15 13:15:49 -0700

    commit tree 3548df0fedf9a047a28368f551ff64515b5da367

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+    fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body:JSON.stringify({name, email})
+    })
+        .then (response => response.json())
+        .then (data => {
+            document.body.innerHTML = data.id
+        })   
+        .catch(error => {
+            document.body.innerHTML = alert(`${message}`)
+            console.log(error.message)
+        })     
+    }
+
+
+
+
+
